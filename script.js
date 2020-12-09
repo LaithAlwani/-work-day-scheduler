210bc7b94a52cb70fcb819fb76e2a80ab6a7936b
Laith Alwani <laithalwani@gmail.com>
2020-12-09 15:16:59 -0500

    saving information to local storage works withe new save and load functions

--- script.js
+++ script.js
@@ -6,22 +6,17 @@ var timeSlot = {
     id: Number,
     time: String,
     event: String
-}
+}  
     
-    
-
-
 var now  = moment();
-
 $("#date").text(now.format("dddd MMMM Do, Y"))
 $("#date").css("font-weight","bold")
-
-
-
+console.log(now.hour());
 
 //this function sets up the page as intended for the view
 function init(){
  for(var i=9 ; i<=17; i++){  //9 comes from 9am and 17 refers to 5pm
+    // appending div and spans and adding classes and attributes
     var row  = $("<div>");
     row.addClass("row no-gutters");
         var timeCol = $("<div>");
@@ -35,7 +30,7 @@ function init(){
         eventCol.addClass("col-md-10 event");
             var eventText = $("<textarea>");
             eventText.addClass("textarea");
-            
+            eventText.attr("id",i-9);
         eventCol.append(eventText);    
 
         var btnCol =$("<div>");
@@ -46,32 +41,96 @@ function init(){
             button.attr("data-id",i-9);
         btnCol.append(button);
         if(now.hour()>i){
-            eventText.addClass("past");
-            eventText.attr("disabled","disabled");
-            button.attr("disabled","disabled")
+            // eventText.addClass("past");
+            // eventText.attr("disabled","disabled");
+            // button.attr("disabled","disabled")
+        }else if(now.hour() == i){
+            // eventText.addClass("present");
+        }else{
+            eventText.addClass("future");
         }
     row.append(timeCol,eventCol,btnCol)
-    
-
     $(".container").append(row);       
-    
  } 
 }
 
+// formats time into am /pm formart
 function formatTime(time){
     var eveningTime = time - 12;
     if(time>12){
-        return (`${eveningTime}pm`);
+        return (`${eveningTime}:00pm`);
     }else{
-        return (`${time}am`);
+        return (`${time}:00am`);
     }
 }
 
 init();
 
-$(".btn").click(function(){
-    var buttonId = ($(this).attr("data-id")); 
-})
+$(".btn").click(buttonClick);
+
+var dataArray = [];
+
+function buttonClick(){
+    var buttonId = ($(this).attr("data-id"));
+    var textAreaId = $(".textarea");
+    for(var i=0 ; i<textAreaId.length;i++){
+        if(textAreaId[i].id === buttonId){
+            console.log(checkId(buttonId));
+            if(typeof checkId(buttonId) === "undefined"){
+                saveNewItem(buttonId, textAreaId[i].value);
+            }
+            else{
+                replaceItem(checkId(buttonId),buttonId,textAreaId[i].value);
+            }
+            
+            console.log(dataArray);
+            localStorage.setItem("data", JSON.stringify(dataArray));
+        }
+    
+    } 
+    
+}
+loadData();
+function loadData(){
+    dataArray = JSON.parse(localStorage.getItem("data"))
+    if(dataArray === null){
+        dataArray = [];
+    }
+    console.log (dataArray);
+    
+}
+
+//check if in data function and returns the index if found
+function checkId(id){
+    console.log(id);
+    var included = false;
+    for(var i=0; i<dataArray.length; i++){
+        if(dataArray[i].id.includes(id)){
+            included = true
+            return (i);
+        }
+        
+    }
+   
+}
+
+//replace function
+
+function saveNewItem(id, value){
+ alert("saving as a new item");
+    dataArray.push({
+        id:id,
+        value:value
+    });
+}
+
+function replaceItem(index, id, value){  
+     dataArray.splice(index,1);
+     dataArray.push({
+         id:id,
+         value:value
+     });
+}
 
 
 
